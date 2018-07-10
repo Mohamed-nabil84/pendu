0ec2e6a5014cb41d90d7811f2508f4c052a85c8d
Mohamed-nabil84 <nabilismail.28@gmail.com>
2018-07-10 13:19:32 +0200

    Initial Commit

---
+++ pendu/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  pendu
+//
+//  Created by Mohamed Nabil ISMAÏL on 10/07/2018.
+//  Copyright © 2018 Mohamed Nabil ISMAÏL. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
